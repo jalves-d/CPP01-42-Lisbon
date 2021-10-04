e670532d541633fdc83ef0b8dcb87a5501278291
jalves-d <jalves-d@student.42lisboa.com>
2021-10-04 17:58:52 +0100

    ex04

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,14 +1,37 @@
 #include <iostream>
 #include <iomanip>
+#include <fstream>
+#include <cstring>
 
-int main(int argc, char **argv)
+void	getdatas(std::string &filestream, std::string &s1, std::string &s2)
 {
+	std::cout << "Digit name of FILESTREAM: ";
+	std::getline(std::cin, filestream);
+	std::cout << "Digit s1: ";
+	std::getline(std::cin, s1);
+	std::cout << "Digit s2: ";
+	std::getline(std::cin, s2);
+}
+
+int main()
+{
+	std::string filestream;
     std::string s1;
     std::string s2;
-    
-    if (argc != 4)
-        return (1);
-    s1 = argv[2];
-    s2 = argv[3];
+	std::ifstream file;
 
+	getdatas(filestream, s1, s2);
+	file.open(filestream.c_str());
+	if (!(file.is_open()))
+		std::cout << "Invalid file ! Does not exist !" << std::endl;
+	if (s1.empty())
+		std::cout << "No s1 identified !" << std::endl;
+	if (s2.empty())
+		std::cout << "No s2 identified !" << std::endl;
+	if (!(file.is_open()) || s2.empty() || s1.empty())
+		return (0);
+	file.close();
+	std::ofstream ofs(filestream.c_str());
+	ofs << s1 << s2 << std::endl;
+	ofs.close();
 }
